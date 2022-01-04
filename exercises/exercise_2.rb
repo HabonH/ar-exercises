3427baa6896ec99298e55548fccd65f00a586b23
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-03 22:57:02 -0500

    Complete Ex 1 to 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -11,7 +11,6 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
-@store3 = Store.find_by(id: 3)
 
 @store1.update(name: 'Toronto')
 @store1.save
